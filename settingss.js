d6c6b95c85b51ce208eeacfcc0357f3675841ea4
kz6212211-blip <kz6212211@gmail.com>
2025-09-04 13:03:30 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ZrtlyZLY#FrINST_69ks_na-kjVyQA5xm6oxdO11ZJxvj5OyKHzg",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "+20 11 53933402", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "كيلواعمك/Harare" //put your country timeZone....leave blank if u don't know.
 };
